f61fb5b64e078250a36b9182935ab2db4bc76d19
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-10-30 20:40:56 +0200

    Get Input value

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import * as ApiRun from './api';
 
+const Search = document.getElementById('search');
+
+const GetInput = () => {
+  const Input = document.getElementById('data').value;
+  console.log(Input);
+};
+
 ApiRun.GetFromAPI();
+
+Search.addEventListener('click', GetInput);
